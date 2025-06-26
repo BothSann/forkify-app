599fc2327db25c2ab75571ce5d7dfad069712278
BothSann <boththann76@gmail.com>
2025-06-26 17:45:28 +0700

    update package.json and config.js for environment variable support; add vercel.json for deployment configuration

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '9c1bd0cc-f38b-454f-9895-89c81bc099f7';
+export const KEY =
+  process.env.VITE_API_KEY || '9c1bd0cc-f38b-454f-9895-89c81bc099f7';
 export const MODAL_CLOSE_SEC = 2.5;
